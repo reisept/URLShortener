edbff62c8b16545d0dcbcc44fac9d9d1556f8562
Eduardo Reis <reise.pt@gmail.com>
2021-09-17 23:31:29 +0100

    general code logic written

--- constants.js
+++ constants.js
@@ -1,19 +1,24 @@
-exports.root_url_port = 3500;
-exports.root_url = "http://localhost:" + root_url_port + "/";
+exports.root_url_port = 9999;
+exports.root_url = "http://localhost:" + this.root_url_port + "/";
 exports.min_vanity_length = 4;
 exports.num_of_urls_per_hour = 50;
 
-exports.get_query = "SELECT * FROM short_urls WHERE short_url = {SEGMENT}";
+exports.get_query = "SELECT * FROM short_urls WHERE short_url = {SHORT_URL}";
 exports.add_query =
-  "INSERT INTO short_urls SET short_url = {SEGMENT}, full_url = {URL}, ip = {IP}, create_ts = now()";
+    "INSERT INTO short_urls SET short_url = {SHORT_URL}, full_url = {URL}, ip = {IP}, create_ts = now()";
 exports.check_url_query = "SELECT * FROM short_urls WHERE url = {URL}";
 
-exports.update_views_query =
-  "UPDATE urls SET num_of_clicks = {VIEWS} WHERE id = {ID}";
-exports.insert_view =
-  "INSERT INTO stats SET ip = {IP}, url_id = {URL_ID}, referer = {REFERER}";
-exports.check_ip_query =
-  "SELECT COUNT(id) as counted FROM urls WHERE datetime_added >= now() - INTERVAL 1 HOUR AND ip = {IP}";
+exports.update_stats_query =
+    "INSERT INTO short_urls_stats (short_url, create_ts, last_access_ts, access_count) " +
+    "VALUES ({SHORT_URL}, now(), now(), 1) " +
+    "ON DUPLICATE KEY UPDATE " +
+    "last_access_ts = now(), access_count = access_count + 1";
+
+exports.get_top100_query =
+    "SELECT s.access_count, s.short_url, u.full_url, s.last_access_ts " +
+    "FROM short_urls_stats s " +
+    "JOIN short_urls u on u.short_url = s.short_url " +
+    "ORDER BY access_count DESC LIMIT 100";
 
 exports.host = "localhost";
 exports.user = "url_shortener";
---
+++ logic.js
@@ -0,0 +1,168 @@
+var mysql = require("mysql");
+var req = require("request");
+var cons = require("./constants");
+var crypto = require("crypto");
+
+var pool = mysql.createPool({
+    host: cons.host,
+    user: cons.user,
+    password: cons.password,
+    database: cons.database,
+});
+
+function generateHash(onSuccess, onError, retryCount, url, request, response, con, vanity) {
+    var hash = "";
+
+    if (vanity) {
+        hash = vanity;
+        var reg = /[^A-Za-z0-9-_]/;
+
+        //If the hash contains invalid characters or is equal to other methods, an error will be thrown
+        if (reg.test(hash) || hash == "add" || hash == "getTop100") {
+            onError(response, request, con, 403);
+            return;
+        }
+        if (hash.length > 15) {
+            onError(response, request, con, 405);
+            return;
+        } else if (cons.min_vanity_length > 0 && hash.length < cons.min_vanity_length) {
+            onError(response, request, con, 407);
+            return;
+        }
+    } else {
+        //This section creates a string for a short URL on basis of an SHA1 hash
+        var shasum = crypto.createHash("sha1");
+        shasum.update(new Date().getTime() + "");
+        hash = shasum.digest("hex").substring(0, 8);
+    }
+
+    //test if the short URL already exists, it will repeat the generateHash function until a short url is generated which does not exist in the database
+    con.query(cons.get_query.replace("{SHORT_URL}", con.escape(hash)), function (err, rows) {
+        if (err) {
+            console.log(err);
+        }
+
+        // not found so its ok to continue
+        if (rows != undefined && rows.length == 0) {
+            onSuccess(hash, url, request, response, con);
+        } else {
+            // already exists generate again
+            if (retryCount > 1 && !vanity) {
+                generateHash(onSuccess, onError, retryCount - 1, url, request, response, con);
+            } else {
+                onError(response, request, con, 400);
+            }
+        }
+    });
+}
+
+//sends a error message back to the client
+function hashError(response, request, con, code) {
+    response.send(urlResult(null, false, code));
+}
+
+//Insert in the database the created short url
+function handleHash(hash, url, request, response, con) {
+    con.query(
+        cons.add_query
+            .replace("{URL}", con.escape(url))
+            .replace("{SHORT_URL}", con.escape(hash))
+            .replace("{IP}", con.escape(getIP(request))),
+        function (err, rows) {
+            if (err) {
+                console.log(err);
+            }
+        }
+    );
+
+    response.send(urlResult(hash, true, 100));
+}
+
+//This function returns the object that will be sent to the client
+function urlResult(hash, result, statusCode) {
+    return {
+        url: hash != null ? cons.root_url + hash : null,
+        result: result,
+        statusCode: statusCode,
+    };
+}
+
+//This method redirects to that URL if it exists
+var getUrl = function (request, response, short_url) {
+    console.log("getUrl: " + short_url);
+
+    pool.getConnection(function (err, con) {
+        // check for errors in getting the connection
+        if (err) {
+            console.log(err);
+            return;
+        }
+        con.query(cons.get_query.replace("{SHORT_URL}", con.escape(short_url)), function (err, rows) {
+            var result = rows;
+            if (!err && rows.length > 0) {
+                // save stats
+                con.query(cons.update_stats_query.replace("{SHORT_URL}", con.escape(short_url)), function (err, rows) {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+                response.redirect(result[0].full_url);
+            } else {
+                response.send(urlResult(null, false, 404));
+            }
+            if (err) {
+                console.log(err);
+            }
+        });
+        con.release();
+    });
+};
+
+//This function adds attempts to add an URL to the database.
+var addUrl = function (request, response, url, vanity) {
+    pool.getConnection(function (err, con) {
+        if (url) {
+            url = decodeURIComponent(url).toLowerCase();
+
+            req(url, function (err, res, body) {
+                if (res != undefined && res.statusCode == 200) {
+                    generateHash(handleHash, hashError, 50, url, request, response, con, vanity);
+                } else {
+                    response.send(urlResult(null, false, 401));
+                }
+            });
+        } else {
+            response.send(urlResult(null, false, 402));
+        }
+        con.release();
+    });
+};
+
+//This function adds attempts to add an URL to the database.
+var getTop100 = function (request, response) {
+    console.log("entrou");
+    pool.getConnection(function (err, con) {
+        con.query(cons.get_top100_query, function (err, rows) {
+            if (err || rows.length == 0) {
+                response.send({ result: false, url: null });
+            } else {
+                response.send({
+                    result: true,
+                    short_url: rows[0].short_url,
+                    full_url: full_url,
+                    clicks: rows[0].access_count,
+                });
+            }
+        });
+        con.release();
+    });
+};
+
+//This function returns the correct IP address. Node.js apps normally run behind a proxy, so the remoteAddress will be equal to the proxy. A proxy sends a header "X-Forwarded-For", so if this header is set, this IP address will be used.
+function getIP(request) {
+    return request.header("x-forwarded-for") || request.connection.remoteAddress;
+}
+
+exports.getUrl = getUrl;
+exports.addUrl = addUrl;
+exports.getTop100 = getTop100;
