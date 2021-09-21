e723b1c1bebf96d60e6b5562642fa527582ebfe2
Eduardo Reis <reise.pt@gmail.com>
2021-09-21 22:13:20 +0100

    .

--- backend/main_app.js
+++ backend/main_app.js
@@ -5,8 +5,8 @@ var constants = require("./constants");
 var cors = require("cors");
 
 app.use(cors());
-app.use(express.bodyParser());
-app.use(express.urlencoded());
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.listen(process.env.PORT || constants.root_url_port);
